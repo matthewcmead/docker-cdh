eaab239b5faef0df1cc8af414fa11562a6277bf0
matthewcmead <matthewcmead@gmail.com>
2018-06-26 07:49:11 -0400

    only spend cycles chowning when necessary

--- cloudera-cdh-namenode/startup.sh
+++ cloudera-cdh-namenode/startup.sh
@@ -1,6 +1,31 @@
 #!/usr/bin/env bash
 
-chown -R hdfs:hadoop /hdfs
+slash_hdfs_owner=$(ls -ld /hdfs | awk '/\/hdfs/ {print $3;}')
+slash_hdfs_group=$(ls -ld /hdfs | awk '/\/hdfs/ {print $4;}')
+dochown=0
+if [ "X$slash_hdfs_owner" != "Xhdfs" ]; then
+  dochown=1
+fi
+if [ "X$slash_hdfs_group" != "Xhadoop" ]; then
+  dochown=1
+fi
+if [ -d /hdfs/nm ]; then
+  slash_hdfs_slash_nm_owner=$(ls -ld /hdfs/nm | awk '/\/hdfs/ {print $3;}')
+  slash_hdfs_slash_nm_group=$(ls -ld /hdfs/nm | awk '/\/hdfs/ {print $4;}')
+  if [ "X$slash_hdfs_slash_nm_owner" != "Xhdfs" ]; then
+    dochown=1
+  fi
+  if [ "X$slash_hdfs_slash_nm_group" != "Xhadoop" ]; then
+    dochown=1
+  fi
+fi
+
+if [ $dochown == 1 ]; then
+  echo "chowning /hdfs recursively to hdfs:hadoop"
+  chown -R hdfs:hadoop /hdfs
+else
+  echo "no need to chown /hdfs -- it is already owned by hdfs:hadoop"
+fi
 
 if [ ! -d /hdfs/nm/current ]; then
   runuser -u hdfs -- hdfs namenode -format
